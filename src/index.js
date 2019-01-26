5f98b15c0c207e198435505afda12d04047ddb35
Mani Jafari <manielli@gmail.com>
2019-01-25 18:37:04 -0800

    Created Product Details and Review Details hard coded HTML component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,50 @@ import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ProductDetails = () => {
+    return (
+        <div>
+            <h1>What is your favourite product on Amazon?</h1>
+            <p>Divine's OG</p>
+            <p>By Original God</p>
+            <p>
+                <small>Seen a million time(s)</small> • 
+                <small>Created 13.56 billion years ago</small> •
+                <small>Last edited just now</small>
+            </p>
+        </div>
+    );
+};
+
+const ReviewDetails = () => {
+    return (
+        <div>
+            <p>Of all the rooms to burn in your uncle's home...The kitchen! Are you mad, boy?</p>
+            <p> 
+                <small>By Ulises Wisozk</small>
+            </p>
+            <p>
+                <small>Created 1 day ago</small>
+            </p>
+        </div>
+    );
+};
+
+const ProductShowPage = () => {
+    return (
+        <div>
+            <ProductDetails />
+            <h1>Reviews</h1>
+            <ReviewDetails />
+        </div>
+    )
+}
+
+
+
+ReactDOM.render(<ProductDetails />, document.getElementById('root'));
+ReactDOM.render(<ReviewDetails />, document.getElementById('root'));
+ReactDOM.render(<ProductShowPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
